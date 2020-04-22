3d6f038a6d6aed12c9072781579b2de2233038b9
KBialecki <36888024+KBialecki@users.noreply.github.com>
2020-04-22 15:44:09 +0200

    Dodano główny skrypt backendu
    Dodawanie pliku przez formularz lub POST.
    Zapisywanie pliku w directory

---
+++ backend/main.py
@@ -0,0 +1,49 @@
+import os
+from flask import Flask, render_template, request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
+
+# na razie uploadowane rzeczy lądują w /AIIR
+UPLOAD_FOLDER = os.path.abspath(os.path.join(os.getcwd(), 'AIIR'))
+# można przefiltrować pliki przed uploadem
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+# mozna wrzucić bezpośrednio przez '/', lub normalnie POSTem
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('Nie wybrano pliku')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Super Apka <3</title>
+    <h1>Gimme the file</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

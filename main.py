7d05076a81c63aea8bfc1eb677a4a1c04de9f44b
Wojciech Stępień <wojciech.tomasz.stepien@gmail.com>
2020-04-29 12:28:03 +0200

    code refactor
    split code into functions

--- main.py
+++ main.py
@@ -1,91 +1,124 @@
 from pyspark.sql import SparkSession
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import OneHotEncoderEstimator, StringIndexer, VectorAssembler
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
+from pyspark.ml.classification import DecisionTreeClassifier
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.classification import GBTClassifier
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
 spark = SparkSession.builder.appName('app').getOrCreate()
-df = spark.read.csv('bank.csv', header = True, inferSchema = True)
+path='bank.csv'
 
-numericCols = [t[0] for t in df.dtypes if t[1] == 'int']
-categoricalColumns = [item[0] for item in df.dtypes if item[1].startswith('string')] 
+def openfile(path):
+    return spark.read.csv(path, header = True, inferSchema = True)
+    
+def checkNumericColumns(df):
+    return [t[0] for t in df.dtypes if t[1] == 'int']
 
-cols = df.columns
+def checkCategoricalColumns(df):
+    return [item[0] for item in df.dtypes if item[1].startswith('string')] 
+
+def allColumns(df):
+    return df.columns
 
 #input
-from pyspark.ml.feature import OneHotEncoderEstimator, StringIndexer, VectorAssembler
-stages = []
-for categoricalCol in categoricalColumns:
-    stringIndexer = StringIndexer(inputCol = categoricalCol, outputCol = categoricalCol + 'Index')
-    encoder = OneHotEncoderEstimator(inputCols=[stringIndexer.getOutputCol()], outputCols=[categoricalCol + "classVec"])
-    stages += [stringIndexer, encoder]
+def indexInputColumns(categoricalColumns,stages):
+    for categoricalCol in categoricalColumns:
+        stringIndexer = StringIndexer(inputCol = categoricalCol, outputCol = categoricalCol + 'Index')
+        encoder = OneHotEncoderEstimator(inputCols=[stringIndexer.getOutputCol()], outputCols=[categoricalCol + "classVec"])
+        stages += [stringIndexer, encoder]
+    return stages,df
 
-#output variable
-label_stringIdx = StringIndexer(inputCol = 'deposit', outputCol = 'label')
-stages += [label_stringIdx]
+#output
+def indexOutputColumn(stages,output):
+    label_stringIdx = StringIndexer(inputCol = output, outputCol = 'label')
+    stages += [label_stringIdx]
+    return stages,df
 
-assemblerInputs = [c + "classVec" for c in categoricalColumns] + numericCols
-assembler = VectorAssembler(inputCols=assemblerInputs, outputCol="features")
-stages += [assembler]
+def vectorAsFeatures(categoricalColumns,numericCols,stages):
+    assemblerInputs = [c + "classVec" for c in categoricalColumns] + numericCols
+    assembler = VectorAssembler(inputCols=assemblerInputs, outputCol="features")
+    stages += [assembler]
+    return stages,df
 
-from pyspark.ml import Pipeline
-pipeline = Pipeline(stages = stages)
-pipelineModel = pipeline.fit(df)
-df = pipelineModel.transform(df)
-selectedCols = ['label', 'features'] + cols
+def pipelane(df,stages,cols):
+    pipeline = Pipeline(stages = stages)
+    pipelineModel = pipeline.fit(df)
+    df = pipelineModel.transform(df)
+    selectedCols = ['label', 'features'] + cols
+    return selectedCols,df
 
-train, test = df.randomSplit([0.7, 0.3], seed = 69)
+def splitDataToTrainAndTest(df):
+    return df.randomSplit([0.7, 0.3], seed = 69)
 
-from pyspark.ml.classification import LogisticRegression
-lr = LogisticRegression(featuresCol = 'features', labelCol = 'label', maxIter=10)
-lrModel = lr.fit(train)
+def logisticRegression(train,selectedCols):
+    selectedCols = selectedCols
+    lr = LogisticRegression(featuresCol = 'features', labelCol = 'label', maxIter=10)
+    lrModel = lr.fit(train)
+    trainingSummary = lrModel.summary
+    print('Training set areaUnderROC: ' + str(trainingSummary.areaUnderROC))
+    predictions = lrModel.transform(test)
+    predictions.select('age','label','rawPrediction','prediction','probability').show(10)
+    return lrModel,lr,predictions
 
-trainingSummary = lrModel.summary
-print('Training set areaUnderROC: ' + str(trainingSummary.areaUnderROC))
+def binaryClassificationEvaluator(predictions):
+    evaluator = BinaryClassificationEvaluator()
+    print("Test Area Under ROC: " + str(evaluator.evaluate(predictions, {evaluator.metricName: "areaUnderROC"})))
+    evaluator.getMetricName()
+    return evaluator
 
-predictions = lrModel.transform(test)
-predictions.select('age','label','rawPrediction','prediction','probability').show(10)
-
-from pyspark.ml.evaluation import BinaryClassificationEvaluator
-evaluator = BinaryClassificationEvaluator()
-print('Test Area Under ROC', evaluator.evaluate(predictions))
-evaluator.getMetricName()
-
-from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
 # Create ParamGrid for Cross Validation
-paramGrid = (ParamGridBuilder()
-             .addGrid(lr.regParam, [0.01, 0.5, 2.0])
-             .addGrid(lr.elasticNetParam, [0.0, 0.5, 1.0])
-             .addGrid(lr.maxIter, [1, 5, 10])
-             .build())
+def paramGridBuilder(lr):
+    return (ParamGridBuilder()
+                 .addGrid(lr.regParam, [0.01, 0.5, 2.0])
+                 .addGrid(lr.elasticNetParam, [0.0, 0.5, 1.0])
+                 .addGrid(lr.maxIter, [1, 5, 10])
+                 .build())
 
-cv = CrossValidator(estimator=lr, estimatorParamMaps=paramGrid, evaluator=evaluator, numFolds=5)
+def crossValidator(lr,paramGrid,evaluator,train,test):
+    cv = CrossValidator(estimator=lr, estimatorParamMaps=paramGrid, evaluator=evaluator, numFolds=5)
+    cvModel = cv.fit(train)
+    predictions = cvModel.transform(test)
+    print('Test Area Under ROC', evaluator.evaluate(predictions))
 
-cvModel = cv.fit(train)
-predictions = cvModel.transform(test)
-print('Test Area Under ROC', evaluator.evaluate(predictions))
+def decisionTreeClassifier(train,test):
+    dt = DecisionTreeClassifier(featuresCol = 'features', labelCol = 'label', maxDepth = 3)
+    dtModel = dt.fit(train)
+    predictions = dtModel.transform(test)
+    predictions.select('age', 'job', 'label', 'rawPrediction', 'prediction', 'probability').show(10)
+    return predictions
 
-from pyspark.ml.classification import DecisionTreeClassifier
+def randomForestClassifier(train,test):
+    rf = RandomForestClassifier(featuresCol = 'features', labelCol = 'label')
+    rfModel = rf.fit(train)
+    predictions = rfModel.transform(test)
+    predictions.select('age', 'job', 'label', 'rawPrediction', 'prediction', 'probability').show(10)
+    return predictions
 
-dt = DecisionTreeClassifier(featuresCol = 'features', labelCol = 'label', maxDepth = 3)
-dtModel = dt.fit(train)
-predictions = dtModel.transform(test)
-predictions.select('age', 'job', 'label', 'rawPrediction', 'prediction', 'probability').show(10)
-
-from pyspark.ml.classification import RandomForestClassifier
-
-rf = RandomForestClassifier(featuresCol = 'features', labelCol = 'label')
-rfModel = rf.fit(train)
-predictions = rfModel.transform(test)
-predictions.select('age', 'job', 'label', 'rawPrediction', 'prediction', 'probability').show(10)
-
-evaluator = BinaryClassificationEvaluator()
-print("Test Area Under ROC: " + str(evaluator.evaluate(predictions, {evaluator.metricName: "areaUnderROC"})))
-evaluator.getMetricName()
-
-from pyspark.ml.classification import GBTClassifier
-
-gbt = GBTClassifier(maxIter=10)
-gbtModel = gbt.fit(train)
-predictions = gbtModel.transform(test)
-predictions.select('age', 'job', 'label', 'rawPrediction', 'prediction', 'probability').show(10)
-
-evaluator = BinaryClassificationEvaluator()
-print("Test Area Under ROC: " + str(evaluator.evaluate(predictions, {evaluator.metricName: "areaUnderROC"})))
-evaluator.getMetricName()
-
+def gbtClassifier(train,test):
+    gbt = GBTClassifier(maxIter=10)
+    gbtModel = gbt.fit(train)
+    predictions = gbtModel.transform(test)
+    predictions.select('age', 'job', 'label', 'rawPrediction', 'prediction', 'probability').show(10)
+    return predictions
+stages=[]
+df=openfile('bank.csv')
+categoricalColumns=checkCategoricalColumns(df)
+numericColumns=checkNumericColumns(df)
+cols=allColumns(df)
+stages,df=indexInputColumns(categoricalColumns,stages)
+stages,df = indexOutputColumn(stages,'deposit')
+stages,df = vectorAsFeatures(categoricalColumns,numericColumns,stages)
+selectedCols,df=pipelane(df,stages,cols)
+train,test = splitDataToTrainAndTest(df)
+lrModel,lr,predictions = logisticRegression(train,selectedCols)
+evaluator = binaryClassificationEvaluator(predictions)
+paramGrid = paramGridBuilder(lr)
+crossValidator(lr,paramGrid,evaluator,train,test)
+predictions = decisionTreeClassifier(train,test)
+binaryClassificationEvaluator(predictions)
+predictions = randomForestClassifier(train,test)
+binaryClassificationEvaluator(predictions)
+predictions = gbtClassifier(train,test)
+binaryClassificationEvaluator(predictions)

1edc6f721268dbc6267fb2497d55aa79812a4b5f
Radosław Stanulewicz <rad8broda@gmail.com>
2020-06-04 14:40:56 +0200

    Add main page

--- backend/main.py
+++ backend/main.py
@@ -36,36 +36,36 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-# # mozna wrzucić bezpośrednio przez '/', lub normalnie POSTem
-# @app.route('/', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         # check if the post request has the file part
-#         if 'file' not in request.files:
-#             flash('No file part')
-#             return redirect(request.url)
-#         file = request.files['file']
-#         # if user does not select file, browser also
-#         # submit an empty part without filename
-#         if file.filename == '':
-#             flash('Nie wybrano pliku')
-#             return redirect(request.url)
-#         if file and allowed_file(file.filename):
-#             filename = secure_filename(file.filename)
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#             return redirect(url_for('upload_file',
-#                                     filename=filename))
-#     return '''
-#     <!doctype html>
-#     <title>Classification engine</title>
-#     <h1>Upload file</h1>
-#     <form method=post enctype=multipart/form-data>
-#       <input type=file name=file>
-#       <input type=submit value=Upload>
-#     </form>
-#     '''
+# mozna wrzucić bezpośrednio przez '/', lub normalnie POSTem
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('Nie wybrano pliku')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Classification engine</title>
+    <h1>Upload file</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
-@app.route('/', methods=['GET'])
+@app.route('/dotask', methods=['GET'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')

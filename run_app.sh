fc50656cd947f0c0eb7ea85504deee6cd0bf37ab
Radosław Stanulewicz <rad8broda@gmail.com>
2020-06-06 20:39:11 +0200

    Change scripts

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 . venv/bin/activate
-cd ./backend
 python main.py
--- run_celery.sh
+++ run_celery.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 . venv/bin/activate
-cd ./backend
 celery worker -A main.celery --loglevel=info --concurrency=1

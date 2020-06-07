d06adc2e1d814c686b3006702d614129049b1a4e
Radosław Stanulewicz <rad8broda@gmail.com>
2020-06-07 22:01:01 +0200

    Add task model

--- server/models.py
+++ server/models.py
@@ -6,3 +6,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id_string = db.Column(db.String(100), unique=True)
